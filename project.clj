edb7be922dbfc9c72db9393483ef588bc3e8f39a
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-05-11 16:28:50 -0700

    Scaffold app Based on default leiningen template

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject portfolio-pie "0.1.0-SNAPSHOT"
+  :description "Provides pie-chart-like composition for portfolio"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "1.0.0"]]
+  :main portfolio-pie.core)
