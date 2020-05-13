cbf92b0ae19daab6049ae6a96c17c3f647ba39e5
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-05-12 19:55:43 -0700

    Style composition summary report

--- src/portfolio_pie/reporting/composition.clj
+++ src/portfolio_pie/reporting/composition.clj
@@ -1,7 +1,20 @@
 (ns portfolio-pie.reporting.composition
-  (:require [portfolio-pie.composition.pie :as composition]
+  (:require [portfolio-pie.composition.pie :refer [by-asset by-type]]
             [portfolio-pie.output.writer :refer [append write]]))
 
+(defn write-summary-header [assets output-file]
+ (write output-file (str (pr-str (by-type assets)) "\n")))
+
+(defn generate-body-line [asset-comp]
+  (str (format "\n%-7s" (first asset-comp)) (second asset-comp)))
+
+(defn append-body-line [output-file asset-comp]
+  (append output-file (generate-body-line asset-comp)))
+
+(defn append-summary-body [assets output-file]
+  (doseq [acomp (by-asset assets)]
+    (append-body-line output-file acomp)))
+
 (defn summarize [assets output-file]
-  (write output-file (pr-str (composition/by-type assets)))
-  (append output-file (pr-str (composition/by-asset assets))))
+  (write-summary-header assets output-file)
+  (append-summary-body assets output-file))
